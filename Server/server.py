12e71b63db7e4ec3b745352362c73d4d5ca208b3
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-10-27 09:45:43 +0100

    create main

---
+++ Server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, json, request
+
+api = Flask(__name__)
+
+temperatures = []
+
+@api.route('/get_temps', methods=['GET'])
+def get_temps():
+  return json.dumps(temperatures)
+
+@api.route('/post_temp', methods=['POST'])
+def post_temp():
+    temperatures.append(str(request.data).replace('b', "").replace("'", ""))
+    print(temperatures)
+    return ' '
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port = 5000)
