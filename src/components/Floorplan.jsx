511cb1a30e09a0f698db4cc74db8aa80dc8247e2
Raaydon <billjrimell@gmail.com>
2022-11-04 22:54:06 +0000

    only working when hosted locally...

--- src/components/Floorplan.jsx
+++ src/components/Floorplan.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { polyfill } from "mobile-drag-drop";
-import $ from "jquery";
+// import $ from "jquery";
 
 polyfill();
 window.addEventListener("touchmove", (e) => {
