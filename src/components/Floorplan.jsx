37db5d8f6dd7070f438b70fee23061066379733b
Bill Rimell <billjrimell@gmail.com>
2022-11-05 12:01:37 +0000

    drag and drop working

--- src/components/Floorplan.jsx
+++ src/components/Floorplan.jsx
@@ -4,7 +4,7 @@ import $ from "jquery";
 export default function Floorplan() {
 	const [diffX, setDiffX] = useState(0);
 	const [diffY, setDiffY] = useState(0);
-	const [draggingStatus, setDraggingStatus] = useState(false);
+	const [dragging, setDragging] = useState(false);
 	const [styles, setStyles] = useState();
 
 	function DraggableItem(props) {
@@ -15,7 +15,6 @@ export default function Floorplan() {
 				draggable
 				style={styles}
 				onMouseDown={dragStart}
-				onMouseMove={dragging}
 				onMouseUp={dragEnd}
 				{...otherProps}
 			>
@@ -25,27 +24,31 @@ export default function Floorplan() {
 	}
 
 	function dragStart(e) {
+		console.log("drag start");
 		setDiffX(e.screenX - e.currentTarget.getBoundingClientRect().left);
 		setDiffY(e.screenY - e.currentTarget.getBoundingClientRect().top);
-		setDraggingStatus(true);
+		setDragging(true);
 	}
 
-	function dragging(e) {
-		if (draggingStatus) {
+	function mouseMove(e) {
+		// console.log(dragging);
+		if (dragging) {
 			var left = e.screenX - diffX;
 			var top = e.screenY - diffY;
 
 			setStyles({
 				left: left,
 				top: top,
+				opacity: "50%",
 				position: "absolute",
 			});
 		}
 	}
 
 	function dragEnd() {
-		setDraggingStatus(false);
-		setStyles({ position: "normal" });
+		console.log("dragend");
+		setDragging(false);
+		// setStyles({ position: "fixed" });
 	}
 
 	function addDraggingStyle(target) {
@@ -55,14 +58,22 @@ export default function Floorplan() {
 		target.classList.remove("dragging");
 	}
 
+	var temperatures = {
+		1: 24,
+		2: 25,
+		3: 19,
+	};
+
 	return (
-		<>
+		<div className="floorplan" onMouseMove={mouseMove}>
 			<img src="/floorplan-example.png" id="floorplan_img" alt="" />
 			<div className="floorplan_items">
-				<DraggableItem>1A</DraggableItem>
-				<DraggableItem>1B</DraggableItem>
-				<DraggableItem>1C</DraggableItem>
+				{Object.entries(temperatures).map((temp) => {
+					return (
+						<DraggableItem key={temp[0]}>{temp[1]}</DraggableItem>
+					);
+				})}
 			</div>
-		</>
+		</div>
 	);
 }
