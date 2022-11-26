d6f5a92db8986538fa12bcf65b0be5451695ff74
Raaydon <billjrimell@gmail.com>
2022-11-26 21:27:36 +0000

    removed battery

--- Client/update_temps.py
+++ Client/update_temps.py
@@ -11,11 +11,11 @@ import ubinascii
 sensor_temp = ADC(4)
 conversion_factor = 3.3 / 65535
  
-vsys = ADC(29)                      # reads the system input voltage
-charging = Pin(24, Pin.IN)          # reading GP24 tells us whether or not USB power is connected
+# vsys = ADC(29)                      # reads the system input voltage
+# charging = Pin(24, Pin.IN)          # reading GP24 tells us whether or not USB power is connected
 
-full_battery = 4.2                  # these are our reference voltages for a full/empty battery, in volts
-empty_battery = 2.8                 # the values could vary by battery size/manufacturer so you might need to adjust them
+# full_battery = 4.2                  # these are our reference voltages for a full/empty battery, in volts
+# empty_battery = 2.8                 # the values could vary by battery size/manufacturer so you might need to adjust them
 
 while True:
     reading = sensor_temp.read_u16() * conversion_factor 
@@ -24,14 +24,14 @@ while True:
     mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
     
     # convert the raw ADC read into a voltage, and then a percentage
-    voltage = vsys.read_u16() * conversion_factor
-    percentage = 100 * ((voltage - empty_battery) / (full_battery - empty_battery))
-    if percentage > 100:
-        percentage = 100.00
+    # voltage = vsys.read_u16() * conversion_factor
+    # percentage = 100 * ((voltage - empty_battery) / (full_battery - empty_battery))
+    # if percentage > 100:
+    #     percentage = 100.00
         
     try:
         payload = {
-            'temperature': str(temperature), 'mac': str(mac), 'battery_percentage': percentage
+            'temperature': str(temperature), 'mac': str(mac)
             }
         # r = requests.post('http://192.168.1.90:3003/post_temp', data = json.dumps(payload))
         r = requests.post('https://tmdash.rimell.cc/api/post_temp', data = json.dumps(payload))
