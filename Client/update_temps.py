eac7242f928cdda471c145155f8a656b1ea9c1e5
Raaydon <billjrimell@gmail.com>
2022-11-26 21:31:57 +0000

    update

--- Client/update_temps.py
+++ Client/update_temps.py
@@ -8,7 +8,7 @@ import json
 import ubinascii
 
 
-sensor_temp = ADC(4)
+temp_sensor = ADC(4)
 conversion_factor = 3.3 / 65535
  
 # vsys = ADC(29)                      # reads the system input voltage
@@ -18,7 +18,7 @@ conversion_factor = 3.3 / 65535
 # empty_battery = 2.8                 # the values could vary by battery size/manufacturer so you might need to adjust them
 
 while True:
-    reading = sensor_temp.read_u16() * conversion_factor 
+    reading = temp_sensor.read_u16() * conversion_factor 
     temperature = round(27 - (reading - 0.706)/0.001721, 2)
     print(temperature)
     mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
@@ -33,8 +33,8 @@ while True:
         payload = {
             'temperature': str(temperature), 'mac': str(mac)
             }
-        # r = requests.post('http://192.168.1.90:3003/post_temp', data = json.dumps(payload))
-        r = requests.post('https://tmdash.rimell.cc/api/post_temp', data = json.dumps(payload))
+        r = requests.post('http://192.168.1.90:3003/post_temp', data = json.dumps(payload))
+        # r = requests.post('https://tmdash.rimell.cc/api/post_temp', data = json.dumps(payload))
         
         # print('free:', str(gc.mem_free()))
         # print('info:', str(gc.mem_alloc()))
