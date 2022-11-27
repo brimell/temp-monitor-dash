588b165a2c4406ef1fbd7d3954136a02113d7392
Raaydon <billjrimell@gmail.com>
2022-11-27 16:50:26 +0000

    update

--- Client/main.py
+++ Client/main.py
@@ -3,24 +3,34 @@ import network
 import utime
 from machine import ADC, Pin, mem32
 
+
 def connectToWiFi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect("daytime", "ryderpawpatrol")
+
+def getCode():
+    url = "https://raw.githubusercontent.com/brimell/temp-monitor-dash/master/Client/update_temps.py"
+    r = requests.get(url)
+    return r
+
+def lightOn():
+    led = Pin("LED", Pin.OUT)
+    led.on()
     
 connectToWiFi()
-print('connected to WiFi')
-utime.sleep(1)
+print("connected to WiFi")
+utime.sleep(5)
 
-print('fetching...')
+print("fetching...")
 
-url = 'https://raw.githubusercontent.com/brimell/temp-monitor-dash/master/Client/update_temps.py'
-r = requests.get(url)
+try:
+    getCode()
+    print("fetched code from github...")
+    # turn on led to indicate finished connection
+    lightOn()
 
-print('fetched code from github...')
-
-# turn on led to indicate finished connection
-led = Pin('LED', Pin.OUT)
-led.on()
-
-exec(r.content)
+    exec(getCode().content)
+except Exception as err:
+    print("failed to get code...")
+    print(err)
