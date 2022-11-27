9a1b1db4d15ad2e4b21f86ee76a1d54627136281
Raaydon <billjrimell@gmail.com>
2022-11-27 19:59:07 +0000

    update

--- Client/main.py
+++ Client/main.py
@@ -7,7 +7,6 @@ from machine import ADC, Pin, mem32
 def connectToWiFi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect("daytime", "ryderpawpatrol")
 
 def getCode():
     url = "https://rimell.cc/bill/update_temps.py"
