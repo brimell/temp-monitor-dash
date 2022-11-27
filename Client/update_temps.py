1dc2cb794f0613e37d148d251e92434f1e32d718
Raaydon <billjrimell@gmail.com>
2022-11-27 17:18:26 +0000

    update

--- Client/update_temps.py
+++ Client/update_temps.py
@@ -47,7 +47,6 @@ while True:
     print("free:", str(gc.mem_free()))
     print("info:", str(gc.mem_alloc()))
     # print("info:", str(micropython.mem_info()))
-    gc.collect()
     reading = temp_sensor.read_u16() * conversion_factor
     temperature = round(27 - (reading - 0.706) / 0.001721, 2)
     print(temperature)
@@ -70,9 +69,8 @@ while True:
         }
         # r = requests.post('http://192.168.1.90:3003/post_temp', data = json.dumps(payload))
         r = requests.post(
-            "https://tmdash.rimell.cc/api/post_temp", data=json.dumps(payload), headers={'Connection':'close'}
+            "https://tmdash.rimell.cc/api/post_temp", data=json.dumps(payload)
         )
-        r.close()
         gc.collect()
     except Exception as e:
         print(e)
