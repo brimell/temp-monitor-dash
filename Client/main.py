44e6f335d5c1cdd89967fd95e790317758119a93
Raaydon <billjrimell@gmail.com>
2022-11-27 11:59:07 +0000

    working battery percentage

--- Client/main.py
+++ Client/main.py
@@ -1,14 +1,13 @@
 import urequests as requests
 import network
 import utime
-from machine import Pin
+from machine import ADC, Pin, mem32
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect("daytime", "ryderpawpatrol")
 utime.sleep(2)
 
-# url = 'https://raw.githubusercontent.com/brimell/temp-monitor-dash/master/Client/update_temps_local.py'
 url = 'https://raw.githubusercontent.com/brimell/temp-monitor-dash/master/Client/update_temps.py'
 r = requests.get(url)
 
--- Client/without_external_connection.py
+++ Client/without_external_connection.py
@@ -1,3 +1,18 @@
+import urequests as requests
+import network
+import utime
+from machine import ADC, Pin, mem32
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("daytime", "ryderpawpatrol")
+utime.sleep(2)
+
+# turn on led to indicate finished connection
+led = Pin('LED', Pin.OUT)
+led.on()
+
+
 import gc
 import json
 import ubinascii
