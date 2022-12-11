3fa0f832518f948c3b2a955bfad8becb2a64bb3a
Raaydon <billjrimell@gmail.com>
2022-12-11 14:22:21 +0000

    Revert "testing"
    This reverts commit 7fdf0a5c36fca5981ae048aa2aa05fd97888f108.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from "react";
-import HomeNotes from "./notes";
 
 export default function Dashboard() {
-	return (
-		<>
-			<HomeNotes />
-		</>
-	);
+	return <>dash</>;
 }
