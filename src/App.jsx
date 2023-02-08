1e93e1ac88079f7a081986afdb679a509cae500a
Raaydon <billjrimell@gmail.com>
2023-02-08 18:47:03 +0000

    added chakra

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, Suspense, lazy, useState } from "react";
 import "./styles/global.scss";
 
 import Loading from "./components/Loading";
-import ReloadPrompt from "./components/ReloadPrompt"
+import ReloadPrompt from "./components/ReloadPrompt";
+
 import { ContextProvider } from "./context/context";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
 import Nav from "./components/nav/nav";
 import { Route, Routes } from "react-router-dom";
 
@@ -11,24 +14,49 @@ const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Floorplan = lazy(() => import("./pages/Floorplan"));
 const Settings = lazy(() => import("./pages/Settings"));
 
+const chakraTheme = extendTheme({
+	styles: {
+		global: {
+			body: {
+				fontFamily: `-apple-system, BlinkMacSystemFont, Inter, Helvetica, Arial,
+              sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+				color: "var(--textColor)",
+				background: "var(--bg)",
+				minHeight: "100vh",
+				width: "100%",
+				padding: 0,
+				margin: 0,
+				overflowX: "hidden",
+				textShadow: "rgba(0, 0, 0, .01) 0 0 1px",
+				textRendering: "optimizeLegibility",
+			},
+		},
+	},
+});
+
 function App() {
 	return (
-		<ContextProvider>
-			<ReloadPrompt />
-			<Suspense fallback={<Loading />}>
-				<div className="App">
-					<Routes>
-						<Route path="/" element={<Floorplan />}></Route>
-						<Route path="/dashboard" element={<Dashboard />}></Route>
-						<Route
-							path="/settings"
-							element={<Settings />}
-						></Route>
-					</Routes>
-				</div>
-				<Nav />
-			</Suspense>
-		</ContextProvider>
+		<ChakraProvider theme={chakraTheme}>
+			<ContextProvider>
+				<ReloadPrompt />
+				<Suspense fallback={<Loading />}>
+					<div className="App">
+						<Routes>
+							<Route path="/" element={<Floorplan />}></Route>
+							<Route
+								path="/dashboard"
+								element={<Dashboard />}
+							></Route>
+							<Route
+								path="/settings"
+								element={<Settings />}
+							></Route>
+						</Routes>
+					</div>
+					<Nav />
+				</Suspense>
+			</ContextProvider>{" "}
+		</ChakraProvider>
 	);
 }
 
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Card, CardHeader, Heading, CardBody, Stack, StackDivider, Box, Text } from "@chakra-ui/react";
+import React from "react";
 
 export default function Settings() {
-  return (
-    <div>Settings</div>
-  )
+	return (
+		<>
+			<Card>
+				<CardHeader>
+					<Heading size="md">Client Report</Heading>
+				</CardHeader>
+
+				<CardBody>
+					<Stack divider={<StackDivider />} spacing="4">
+						<Box>
+							<Heading size="xs" textTransform="uppercase">
+								Summary
+							</Heading>
+							<Text pt="2" fontSize="sm">
+								View a summary of all your clients over the last
+								month.
+							</Text>
+						</Box>
+						<Box>
+							<Heading size="xs" textTransform="uppercase">
+								Overview
+							</Heading>
+							<Text pt="2" fontSize="sm">
+								Check out the overview of your clients.
+							</Text>
+						</Box>
+						<Box>
+							<Heading size="xs" textTransform="uppercase">
+								Analysis
+							</Heading>
+							<Text pt="2" fontSize="sm">
+								See a detailed analysis of all your business
+								clients.
+							</Text>
+						</Box>
+					</Stack>
+				</CardBody>
+			</Card>
+		</>
+	);
 }
